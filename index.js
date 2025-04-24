da45cd86a327c933428c11251ff8101a20480d4b
Vaibhav1511 <meet.vaibhav09@gmail.com>
2025-04-24 12:08:01 +0530

    fix: removed extra CORS variable for Vercel frontend

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-const cors = require('cors');
-
 app.use(cors({
   origin: 'https://upsc-question-bank-frontend.vercel.app', // allow Vercel frontend
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
